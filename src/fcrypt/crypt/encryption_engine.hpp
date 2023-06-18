10d18d36bcecac9fe7056fe00b5b58933aa74bfa
MateuszJanduraUszu <Mati4321@interia.pl>
2023-06-18 22:05:26 +0200

    `<encryption_engine>`: Make `encryption_engine` a base class for all encryption engines

--- src/fcrypt/crypt/encryption_engine.hpp
+++ src/fcrypt/crypt/encryption_engine.hpp
@@ -8,62 +8,35 @@
 #define _FCRYPT_CRYPT_ENCRYPTION_ENGINE_HPP_
 #include <fcrypt/app/utils.hpp>
 #include <cstddef>
-#include <openssl/evp.h>
 
 namespace fcrypt {
     using key                = _Secure_buffer<32>;
     using iv                 = _Secure_buffer<12>;
     using authentication_tag = _Secure_buffer<16>;
 
-    class _Cipher_context {
-    public:
-        _Cipher_context() noexcept;
-        ~_Cipher_context() noexcept;
+    // Note: Currently, we support the AES-256-GCM encryption engine only. However, we keep
+    //       the "encryption_enigne" as an abstract class to allow for future expansion
+    //       and the addition of other encryption engines if needed.
 
-        static constexpr size_t _Tag_size = authentication_tag::size;
-
-        // checks if the context is valid
-        bool _Valid() const noexcept;
-
-        // returns a pointer to the context
-        EVP_CIPHER_CTX* _Get() noexcept;
-
-        // changes the authentication tag associated with the context
-        bool _Get_tag(byte_t* const _Tag) noexcept;
-
-        // returns the authentication tag associated with the context
-        bool _Set_tag(byte_t* const _New_tag) noexcept;
-
-    private:
-        EVP_CIPHER_CTX* _Myptr;
-    };
-
-    class encryption_engine { // AES-256-GCM encryption engine
+    class __declspec(novtable) encryption_engine { // base class for all encryption engines
     public:
         encryption_engine() noexcept;
-        ~encryption_engine() noexcept;
-    
-        // tries to setup the encryption process
-        bool setup_encryption(const key& _Key, const iv& _Iv) noexcept;
+        virtual ~encryption_engine() noexcept;
 
-        // tries to setup the decryption process
-        bool setup_decryption(const key& _Key, const iv& _Iv) noexcept;
-    
-        // tries to encrypt the data
-        bool encrypt(const byte_t* const _Data, const size_t _Size, byte_t* const _Buf) noexcept;
-    
-        // tries to decrypt the data
-        bool decrypt(const byte_t* const _Data, const size_t _Size, byte_t* const _Buf) noexcept;
-    
-        // tries to complete the encryption process
-        bool complete_encryption(authentication_tag& _Tag) noexcept;
-    
-        // tries to complete the decryption process
-        bool complete_decryption(authentication_tag& _Tag) noexcept;
+        enum id : unsigned char {
+            none,
+            aes256_gcm
+        };
 
-    private:
-        _Cipher_context _Myctx;
+        virtual bool setup_encryption(const key&, const iv&) noexcept                   = 0;
+        virtual bool setup_decryption(const key&, const iv&) noexcept                   = 0;
+        virtual bool encrypt(const byte_t* const, const size_t, byte_t* const) noexcept = 0;
+        virtual bool decrypt(const byte_t* const, const size_t, byte_t* const) noexcept = 0;
+        virtual bool complete_encryption(authentication_tag&) noexcept                  = 0;
+        virtual bool complete_decryption(authentication_tag&) noexcept                  = 0;
     };
+
+    [[nodiscard]] encryption_engine* make_encryption_engine(const encryption_engine::id _Id) noexcept;
 } // namespace fcrypt
 
 #endif // _FCRYPT_CRYPT_ENCRYPTION_ENGINE_HPP_
